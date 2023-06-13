8d3c49a16c118bacd661657e800b6888a203ecec
printfCYQ <459987870@qq.com>
2023-06-13 09:03:21 +0800

    WeixinJSBridge close window

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,8 +2,28 @@
     <div class="mt-5 ml-10" v-for="route in routes" :key="route.path">
         <router-link :to="route.path">{{ route.name }}</router-link>
     </div>
+    <button @click="close">close</button>
 </template>
 
 <script setup lang="ts">
 import { routes } from '@/router';
+
+const close = () => {
+    setTimeout(function () {
+        //这个可以关闭安卓系统的手机
+        document.addEventListener(
+            "WeixinJSBridgeReady",
+            function () {
+                WeixinJSBridge.call("closeWindow");
+            },
+            false
+        );
+        //这个可以关闭ios系统的手机
+        WeixinJSBridge.call("closeWindow");
+
+        if (!WeixinJSBridge) {
+            window.close()
+        }
+    }, 300)
+}
 </script>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
 declare let AMap: any;
+declare let WeixinJSBridge: any;
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
